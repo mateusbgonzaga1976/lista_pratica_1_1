bdd5cd270f187f4f583b8a92b5f5d49276c18942
mateusbgonzaga1976 <55139704+mateusbgonzaga1976@users.noreply.github.com>
2019-09-10 09:06:27 -0300

    Add files via upload
    exercício 6 da primeira lista prática de técnicas de desenvolvimento de algoritmo.

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main()
+int main(void)
 {
-    int ano, anoBissexto;
+    float n1, n2, n3, p1, p2, media;
 
-    printf("Digite o ano a ser consultado: ");
-    scanf("%d", &ano);
+        p1 = 1.0;
+        p2 = 2.0;
 
-        anoBissexto =  ano % 2;
+        printf("Digite a primeira nota: ");
+        scanf("%f", &n1);
 
-    if (anoBissexto == 0){
-        printf("%d eh um Ano Bissexto\n", ano);
+        printf("Digite a segunda nota: ");
+        scanf("%f", &n2);
+
+        printf("Digite a terceira nota: ");
+        scanf("%f", &n3);
+
+        media = ((n1*p1) + (n2*p1) + (n3*p2))/(p1+p2);
+
+        printf("media = %.2f\n\n", media);
+
+    if (media >= 6.0){
+        printf("O aluno foi aprovado com media:%.2f\n ", media);
     }
-    else{
-        printf("%d nao eh um Ano Bissexto\n", ano);
-    }
-
     system("pause");
     return 0;
 }
