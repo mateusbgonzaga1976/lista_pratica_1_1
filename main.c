5966936c6ef27eb8ab0dfb3ba3caf1878aa772ea
mateusbgonzaga1976 <55139704+mateusbgonzaga1976@users.noreply.github.com>
2019-09-10 09:11:04 -0300

    Add files via upload
    exercício 8 da lista prática de técnica de desenvolvimento de algoritmo

--- main.c
+++ main.c
@@ -3,29 +3,9 @@
 
 int main()
 {
-    float n1, n2, media;
-
-    printf("Digite a primeira nota do aluno: ");
-    scanf("%f", &n1);
-
-    printf("Digite a segunda nota do aluno: ");
-    scanf("%f", &n2);
-
-    if (n1 < 0.0 || n1 > 10.0){
-        printf("A nota n1 %.2f, nao e valida.\n", n1);
-    }
-    if (n2 < 0.0 || n2 > 10.0){
-            printf("A nota n2 %.2f, nao e valida.\n", n2);
-    }
-    else{
-        if (n1 >= 0.0 && n1 <= 10.0 ){
-            if (n2 >= 0.0 && n2 <= 10.0 ){
-                media = (n1 + n2)/2;
-                printf("media = %.2f\n", media);
-            }
-        }
-    }
-
+    printf("Mateus Gonzaga;\n");
+    int matricula = 1920027391;
+    printf ("matricula %2X\n",matricula,matricula);
     system("pause");
     return 0;
 }
