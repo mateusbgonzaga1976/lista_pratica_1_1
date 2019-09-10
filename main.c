3732dbf306271ba16e0b632190638cf2aa785238
mateusbgonzaga1976 <55139704+mateusbgonzaga1976@users.noreply.github.com>
2019-09-10 09:02:31 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <conio.h>
 
 int main()
 {
-    printf("Mateus Gonzaga;\n");
-    int matricula = 1920027391;
-    printf ("matricula %2X\n",matricula,matricula);
+    int B;
+    float A, resultado;
+
+        printf("Digite o valor da base:\n");
+        scanf("%f", &A);
+        printf("Digite o valor do indice:\n");
+        scanf("%d", &B);
+            resultado = pow(A, B);
+        printf("O resultado eh: %*1.7e10\n", resultado);
+
     system("pause");
     return 0;
 }
