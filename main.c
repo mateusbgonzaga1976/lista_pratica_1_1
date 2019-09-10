b0b91c48ab451f354d278948728441af44b2c753
mateusbgonzaga1976 <55139704+mateusbgonzaga1976@users.noreply.github.com>
2019-09-10 08:57:59 -0300

    Add files via upload
    Lista prática 1 de exercício.
    questões de 2 a 8
    curso de técnicas de desenvolvimento de algoritmos.
    prof Leando

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Mateus Gonzaga;\n");
+    int matricula = 1920027391;
+    printf ("matricula %2X\n",matricula,matricula);
+    system("pause");
+    return 0;
+}
