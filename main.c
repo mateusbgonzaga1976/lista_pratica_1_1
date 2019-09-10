7e56e2dc5dda61439c9ee3be83aba2b91e782b3c
mateusbgonzaga1976 <55139704+mateusbgonzaga1976@users.noreply.github.com>
2019-09-10 09:10:22 -0300

    Add files via upload
    exercício 7 da lista prática de técnica de desenvolvimento de algoritmo

--- main.c
+++ main.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int main(void)
+int main()
 {
-    float n1, n2, n3, p1, p2, media;
+    float n1, n2, media;
 
-        p1 = 1.0;
-        p2 = 2.0;
+    printf("Digite a primeira nota do aluno: ");
+    scanf("%f", &n1);
 
-        printf("Digite a primeira nota: ");
-        scanf("%f", &n1);
+    printf("Digite a segunda nota do aluno: ");
+    scanf("%f", &n2);
 
-        printf("Digite a segunda nota: ");
-        scanf("%f", &n2);
-
-        printf("Digite a terceira nota: ");
-        scanf("%f", &n3);
-
-        media = ((n1*p1) + (n2*p1) + (n3*p2))/(p1+p2);
-
-        printf("media = %.2f\n\n", media);
-
-    if (media >= 6.0){
-        printf("O aluno foi aprovado com media:%.2f\n ", media);
+    if (n1 < 0.0 || n1 > 10.0){
+        printf("A nota n1 %.2f, nao e valida.\n", n1);
     }
+    if (n2 < 0.0 || n2 > 10.0){
+            printf("A nota n2 %.2f, nao e valida.\n", n2);
+    }
+    else{
+        if (n1 >= 0.0 && n1 <= 10.0 ){
+            if (n2 >= 0.0 && n2 <= 10.0 ){
+                media = (n1 + n2)/2;
+                printf("media = %.2f\n", media);
+            }
+        }
+    }
+
     system("pause");
     return 0;
 }
